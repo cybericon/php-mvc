c91847ca8c61227a68303a9d90cc31c35db6e876
Muhammad Furqan <drmfurqan@gmail.com>
2018-10-09 22:16:15 +0500

    refactored task model

--- app/models/Task.php
+++ app/models/Task.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
+use Core\App;
 use Core\Database\Model;
 
 class Task extends Model
 {
-    protected static $table = 'todos';
+    protected static $table = 'tasks';
+    public $description;
+    public $details;
+    public $completed;
+    public $id;
 
+    public function addTask()
+    {
+        $details = $this->details;
+        $description = $this->description;
+        $this->add(compact('description', 'details'));
+    }
+
+    public static function allTasks()
+    {
+        return static::get();
+    }
+
+    public function isComplete($id)
+    {
+        //
+    }
+
+    public function showTask($id)
+    {
+        return static::find($id);
+    }
 }
